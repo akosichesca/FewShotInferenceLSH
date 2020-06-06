ad5667dab5058b8fc61fed93f853c351eca98954
akosichesca <akosichesca@gmail.com>
2020-06-05 20:56:15 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # FewShotInference
 
 python setup.py install
+
 bash download_omniglot.sh
+
 python run_eval.py --dist.qbits=8 --data.test_shot=1 --data.test_way=5
--- download_omniglot.sh
+++ download_omniglot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DATADIR=data/omniglot/data
+
 mkdir -p $DATADIR
 wget -O images_background.zip https://github.com/brendenlake/omniglot/blob/master/python/images_background.zip?raw=true
 wget -O images_evaluation.zip https://github.com/brendenlake/omniglot/blob/master/python/images_evaluation.zip?raw=true
